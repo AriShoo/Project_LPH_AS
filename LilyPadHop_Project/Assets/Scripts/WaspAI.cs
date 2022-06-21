87234b40fb380e823d95a7f311e515073ad4cc14
AriShoo <al_sherlock95@outlook.com>
2022-06-21 23:54:32 +0100

    Added Enemy
    Added in the wasp enemy, animated and  patrolling as it should be.

---
+++ LilyPadHop_Project/Assets/Scripts/WaspAI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaspAI : MonoBehaviour
+{
+
+    public float speed = 0.8f;
+    public float range = 3;
+
+    float startingX;
+    int dir = 1; 
+
+    void Start()
+    {
+        startingX = transform.position.x;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime * dir);
+        if (transform.position.x < startingX || transform.position.x > startingX + range)
+        {
+            dir *= -1;
+        }
+
+    }
+}
